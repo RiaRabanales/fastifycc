457db12741e0ffeea6b3e294a302c55b09b37564
Maria Rabanales <maria.rabanales@aireuropa.com>
2023-04-28 15:39:22 +0200

    clean routes

--- routes/items.js
+++ routes/items.js
@@ -1,50 +1,43 @@
-const items = require('../Items')
+const { getItems, getItem } = require('../controllers/items')
 
-// Opciones para get all items -> formatea el schema para la respuesta de la llamada en caso ok
+// Item schema
+const Item =  {
+    type: 'object',
+    properties: {
+        id: {type: 'integer'},   // si borrara esta línea no devolvería ids; si pusiera 'string' devolvería tipo string
+        name: {type: 'string'}
+    }
+}
+
+// Opciones para get -> formatea el schema para la respuesta de la llamada en caso ok
 const getItemsOpts = {
     schema: {
         response: {
             200: {
                 type: 'array',
-                items: {
-                    type: 'object',
-                    properties: {
-                        id: {type: 'integer'},   // si borrara esta línea no devolvería ids; si pusiera 'string' devolvería tipo string
-                        name: {type: 'string'}
-                    }
-                }
+                items: Item
             }
         }
-    }
+    },
+    handler: getItems
 }
 
 const getItemOpts = {
     schema: {
         response: {
-            200: {
-                type: 'object',
-                properties: {
-                    id: {type: 'integer'},
-                    name: {type: 'string'}
-                }
-            }
+            200: Item
         }
-    }
+    },
+    handler: getItem
 }
 
 //al separar las rutas, puedo registrarlas como un plugin
 function itemRoutes (fastify, options, done) {
     // Get all items
-    fastify.get('/items', getItemsOpts, (req, reply) => {
-        reply.send(items)
-    })
+    fastify.get('/items', getItemsOpts)
     
     // Get single items
-    fastify.get('/items/:id', getItemOpts, (req, reply) => {
-        const {id} = req.params
-        const item = items.find(item => item.id === id)
-        reply.send(item)
-    })
+    fastify.get('/items/:id', getItemOpts)
     done()
 }
 
